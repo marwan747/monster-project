5e61e11a5770d5a919b3590ea83fc0eb5345a9fc
marwan <marwan_bolbo747@hotmail.com>
2020-10-06 18:37:38 +0200

    Updates

---
+++ precache-manifest.4cbf8a9772e1cf99088877bd302f38cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ce3665e9caded8c751c7b8003d71596",
+    "url": "/monster-project/index.html"
+  },
+  {
+    "revision": "d744ecb9a3b32cbe0a4e",
+    "url": "/monster-project/static/css/main.356785ee.chunk.css"
+  },
+  {
+    "revision": "d9d611a3581dc57a06f7",
+    "url": "/monster-project/static/js/2.ecc30aa7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-project/static/js/2.ecc30aa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d744ecb9a3b32cbe0a4e",
+    "url": "/monster-project/static/js/main.01f19d2b.chunk.js"
+  },
+  {
+    "revision": "944cd0ae4e2d63e25d61",
+    "url": "/monster-project/static/js/runtime-main.cde74da7.js"
+  }
+]);
